02fa348815f4acef760af8cd04d9d63d5fcf8681
Paweł Karpiński <karpinski.pawel.piotr@gmail.com>
2023-01-08 00:40:37 +0100

    animation update

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require('browser-sync').create();
+const entryPath = ".";
+
+
+function server(cb) {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+    cb();
+}
+
+
+function css() {
+    return gulp.src('./scss/main.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            // outputStyle: 'compressed' //compressed
+        }).on('error', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(sourcemaps.write("."))
+        .pipe(gulp.dest('./css'))
+        .pipe(browserSync.stream());
+}
+
+function watch(cb) {
+    gulp.watch("./scss/**/*.scss", gulp.parallel(css));
+    gulp.watch("./*.html").on('change', browserSync.reload);
+    gulp.watch(entryPath + "/js/*.js", browserSync.reload);
+    cb();
+}
+
+module.exports.css = css;
+module.exports.watch = watch;
+module.exports.server = server;
+module.exports.default = gulp.series(css, watch, server);
